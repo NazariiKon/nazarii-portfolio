7206bb1239b9a2ae2520a7c68fe211d3464617fd
Nazarii Konechnyi <nazar.konechniy2@gmail.com>
2026-02-04 21:44:06 +0000

    init commit

---
+++ src/router/main.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Main from "../pages/main";
+
+export default function AppRouter() {
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Main />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
